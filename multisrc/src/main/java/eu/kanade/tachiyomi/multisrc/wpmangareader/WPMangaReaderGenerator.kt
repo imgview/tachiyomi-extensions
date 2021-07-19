4bf119006e062c1c47963ccf8a45908db1469977
Riztard <riyanluqman@gmail.com>
2021-07-19 19:36:18 +0700

    add flamescansx, update kotlinx dependencies

---
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpmangareader/WPMangaReaderGenerator.kt
@@ -0,0 +1,24 @@
+package eu.kanade.tachiyomi.multisrc.wpmangareader
+
+import generator.ThemeSourceData.SingleLang
+import generator.ThemeSourceGenerator
+
+class WPMangaReaderGenerator : ThemeSourceGenerator {
+
+    override val themePkg = "wpmangareader"
+
+    override val themeClass = "WPMangaReader"
+
+    override val baseVersionCode: Int = 8
+
+    override val sources = listOf(
+        SingleLang("Flame Scans X", "https://flamescans.org", "en"),
+    )
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            WPMangaReaderGenerator().createAll()
+        }
+    }
+}
