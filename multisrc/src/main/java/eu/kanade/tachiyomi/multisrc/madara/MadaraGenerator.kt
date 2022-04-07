cd25788684f57bba931d13f1f8db995a992ef72d
Riztard <riyanluqman@gmail.com>
2022-04-07 17:04:33 +0700

    Add Madara + DragonTea

---
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
@@ -0,0 +1,25 @@
+package eu.kanade.tachiyomi.multisrc.madara
+
+import generator.ThemeSourceData.MultiLang
+import generator.ThemeSourceData.SingleLang
+import generator.ThemeSourceGenerator
+
+class MadaraGenerator : ThemeSourceGenerator {
+
+    override val themePkg = "madara"
+
+    override val themeClass = "Madara"
+
+    override val baseVersionCode: Int = 18
+
+    override val sources = listOf(
+        SingleLang("DragonTea", "https://dragontea.ink", "en", overrideVersionCode = 1),
+    )
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            MadaraGenerator().createAll()
+        }
+    }
+}
