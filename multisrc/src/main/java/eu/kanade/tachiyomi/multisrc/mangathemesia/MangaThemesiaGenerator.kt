6235d59fdf22f6943c94f8fcfe5fe13b01bebb8f
Riztard <riyanluqman@gmail.com>
2022-11-21 16:20:53 +0700

    Luminous Scans: update url, add space in details

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangathemesia/MangaThemesiaGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangathemesia/MangaThemesiaGenerator.kt
@@ -10,12 +10,12 @@ class MangaThemesiaGenerator : ThemeSourceGenerator {
 
     override val themeClass = "MangaThemesia"
 
-    override val baseVersionCode: Int = 21
+    override val baseVersionCode: Int = 22
 
     override val sources = listOf(
         SingleLang("Asura Scans X", "https://www.asurascans.com", "en", overrideVersionCode = 5),
         SingleLang("Flame Scans X", "https://flamescans.org", "en", overrideVersionCode = 1),
-        SingleLang("Luminous Scans", "https://www.luminousscans.com", "en", overrideVersionCode = 2),
+        SingleLang("Luminous Scans", "https://luminousscans.com", "en", overrideVersionCode = 2),
     )
 
     companion object {
