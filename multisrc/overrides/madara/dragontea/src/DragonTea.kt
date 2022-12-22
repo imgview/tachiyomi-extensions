f640ed036a44735f44bb76212f8f7c1a559a0ca9
Riztard <riyanluqman@gmail.com>
2022-12-22 23:17:44 +0700

    DragonTea: add again
    changing header

--- multisrc/overrides/madara/dragontea/src/DragonTea.kt
+++ multisrc/overrides/madara/dragontea/src/DragonTea.kt
@@ -34,8 +34,8 @@ class DragonTea : Madara(
         .build()
 
     override fun headersBuilder(): Headers.Builder = Headers.Builder()
-        .add("User-Agent", "Googlebot/2.1 (+http://www.googlebot.com/bot.html)")
-        .add("Referer", "https://www.google.com")
+        .add("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Mobile Safari/537.36")
+        .add("Referer", "$baseUrl/")
 
     override val useNewChapterEndpoint = true
 
