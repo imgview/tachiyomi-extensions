3ca5011dec55e73a50116c748505cb6489ac6eff
Riztard Lanthorn <riyanluqman@gmail.com>
2023-10-26 22:04:15 +0700

    add rate limit

--- multisrc/overrides/mangathemesia/rizzcomics/src/RizzComics.kt
+++ multisrc/overrides/mangathemesia/rizzcomics/src/RizzComics.kt
@@ -1,13 +1,21 @@
 package eu.kanade.tachiyomi.extension.en.rizzcomics
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import okhttp3.OkHttpClient
 import java.text.SimpleDateFormat
 import java.util.Locale
+import java.util.concurrent.TimeUnit
 
 class RizzComics : MangaThemesia(
     "Rizz Comics",
     "https://rizzcomic.com",
-    "en", 
+    "en",
     "/series",
-    SimpleDateFormat("dd MMM yyyy", Locale.US)
-)
+    SimpleDateFormat("dd MMM yyyy", Locale.US),
+) {
+
+    override val client: OkHttpClient = super.client.newBuilder()
+        .rateLimit(20, 5, TimeUnit.SECONDS)
+        .build()
+}
