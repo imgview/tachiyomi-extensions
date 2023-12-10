72ef2c0773738516cfa8e84c7052d59973feebf9
Riztard Lanthorn <16263232+Riztard@users.noreply.github.com>
2023-12-10 19:06:10 +0700

    Reset Scans: add rate limit

--- multisrc/overrides/madara/resetscans/src/ResetScans.kt
+++ multisrc/overrides/madara/resetscans/src/ResetScans.kt
@@ -1,8 +1,15 @@
 package eu.kanade.tachiyomi.extension.en.resetscans
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
 
 class ResetScans : Madara("Reset Scans", "https://reset-scans.us", "en") {
+
+    override val client: OkHttpClient = super.client.newBuilder()
+        .rateLimit(20, 5, TimeUnit.SECONDS)
+        .build()
 
     override val mangaDetailsSelectorDescription = ".description-summary > .summary__content"
 
